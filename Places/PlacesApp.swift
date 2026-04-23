1bc1d330da61512f9edef7637c95b736b66cab34
Mahyar <shakourimahyar@outlook.com>
2026-04-23 23:16:01 +0200

    Initial Commit

---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Mahyar on 23/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
