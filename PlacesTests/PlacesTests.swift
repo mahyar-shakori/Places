608223a7b90d86d1287303e283c82f6511a36356
Mahyar <shakourimahyar@outlook.com>
2026-04-23 23:35:06 +0200

    Setup initial project structure

---
+++ PlacesTests/PlacesTests.swift
@@ -0,0 +1,18 @@
+//
+//  PlacesTests.swift
+//  PlacesTests
+//
+//  Created by Mahyar on 23/04/2026.
+//
+
+import Testing
+
+struct PlacesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
